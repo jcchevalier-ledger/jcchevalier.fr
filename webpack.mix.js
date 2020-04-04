e68939b08d77ef6b283e038d5ac354f14dca6a1f
jisse <jean.christophe.chevalier.3@gmail.com>
2020-04-04 02:08:59 +0200

    modified .gitignore
    added /public/images
    images are now stocked inside the resources file, and copied upon npm
    run
    
    Signed-off-by: jisse <jean.christophe.chevalier.3@gmail.com>

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,11 +12,13 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js');
+mix.sass('resources/sass/app.scss', 'public/css');
+
 mix.js('resources/js/global/loading.js', 'public/js');
 mix.js('resources/js/global/navbar.js', 'public/js');
 mix.js('resources/js/landing_page/landing_page.js', 'public/js');
 
 mix.sass('resources/sass/landing_page/landing_page.scss', 'public/css');
-
 mix.sass('resources/sass/global/global.scss', 'public/css');
-mix.sass('resources/sass/app.scss', 'public/css');
+
+mix.copyDirectory('resources/images', 'public/images');
