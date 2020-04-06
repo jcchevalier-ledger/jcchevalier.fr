6f6b44d4e69c1edceebdb1589e8b8d80ce9fa6b4
jisse <jean.christophe.chevalier.3@gmail.com>
2020-04-06 18:41:56 +0200

    [Update navbar.js]
    Navbar will now be automatically fixed top if the document does not
    contain an element with "about-me" as ID (ie landing-page)
    
    Signed-off-by: jisse <jean.christophe.chevalier.3@gmail.com>

--- resources/js/global/navbar.js
+++ resources/js/global/navbar.js
@@ -1,9 +1,16 @@
-window.onscroll = function () {
-    add_sticky();
-};
-
 const navbar = document.getElementById("navbar");
 const aboutMe = document.getElementById("about-me");
+
+window.onload = function () {
+    if (aboutMe != null) {
+        window.addEventListener('scroll', function () {
+            add_sticky();
+        });
+    } else {
+        navbar.classList.add("fixed-top");
+    }
+};
+
 
 function add_sticky() {
 
