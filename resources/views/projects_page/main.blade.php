676fbe9b71fdab9c116dc7e250127dc86a30a4d5
jisse <jean.christophe.chevalier.3@gmail.com>
2020-04-06 18:36:27 +0200

    [added basic front end]
    Added necessary blade template and SCSS files
    
    Signed-off-by: jisse <jean.christophe.chevalier.3@gmail.com>

--- resources/views/projects_page/main.blade.php
+++ resources/views/projects_page/main.blade.php
@@ -3,6 +3,7 @@
 @section('title', 'My projects')
 
 @section('additional_css')
+    <link href="{{ mix('/css/projects_page.css') }}" rel="stylesheet"/>
 @endsection
 
 @section('start_scripts')
@@ -10,4 +11,36 @@
 
 @section('navbar')
     @parent
+@endsection
+
+@section('content')
+    <div class="background-1">
+        <div class="jumbotron central">
+
+            <h1>My technicals projects</h1>
+            <hr>
+            <p>On this web page, you can interact with my personal projects that have helped me to develop my skills in
+                certain areas of IT</p>
+
+            <div class="projects">
+
+                <div class="row">
+
+                    <div class="col-sm-6">
+                        <p>Description here</p>
+                        <button type="button" id="login-interface" class="btn btn-outline-success btn-lg">Access the
+                            login interface
+                        </button>
+                    </div>
+
+                    <div class="col-sm-6">
+                        <p>Description here</p>
+                        <button type="button" class="btn btn-outline-success btn-lg">Access the project n°2</button>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+    </div>
 @endsection
--- webpack.mix.js
+++ webpack.mix.js
@@ -19,6 +19,7 @@ mix.js('resources/js/global/navbar.js', 'public/js');
 mix.js('resources/js/landing_page/landing_page.js', 'public/js');
 
 mix.sass('resources/sass/landing_page/landing_page.scss', 'public/css');
+mix.sass('resources/sass/projects_page/projects_page.scss', 'public/css');
 mix.sass('resources/sass/global/global.scss', 'public/css');
 
 mix.copyDirectory('resources/images', 'public/images');
