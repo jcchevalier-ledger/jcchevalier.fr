62169aebd5b3f5f030544b5bd79c3d02d572b10e
jisse <jean.christophe.chevalier.3@gmail.com>
2020-04-08 08:00:58 +0200

    [navbar modification]
    removed "scroll" event listener on navbar.js
    the display of the navbar-brand class is now handled in JS, according
    to the inner width of the windows and the number of "nav-items"
    
    Signed-off-by: jisse <jean.christophe.chevalier.3@gmail.com>

--- resources/views/projects_page/main.blade.php
+++ resources/views/projects_page/main.blade.php
@@ -20,16 +20,14 @@
             <h1>My technicals projects</h1>
             <hr>
             <p>On this web page, you can interact with my personal projects that have helped me to acquire some skills
-                in
-                web development</p>
+                in web development</p>
 
             <div class="projects">
 
                 <div class="row">
 
                     <div class="col-sm-6">
-                        <p>The objective of this project was to code a fully functional login interface. It also
-                            respects GDPR legislation</p>
+                        <p>The objective of this project was to code a fully functional login interface</p>
                         <button type="button" id="login-interface" class="btn btn-outline-success btn-lg">Access the
                             login interface
                         </button>
