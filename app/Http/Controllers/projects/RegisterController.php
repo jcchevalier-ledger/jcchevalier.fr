e0ac5aa69719558a626eff0426ff9751e22d4cd1
jisse <jean.christophe.chevalier.3@gmail.com>
2020-04-19 00:51:20 +0200

    [documentation]
    added missing PHPDoc for the RegisterController.php
    
    Signed-off-by: jisse <jean.christophe.chevalier.3@gmail.com>

--- app/Http/Controllers/projects/RegisterController.php
+++ app/Http/Controllers/projects/RegisterController.php
@@ -5,20 +5,27 @@ namespace App\Http\Controllers\projects;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\RegisterRequest;
 use App\User;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
+/**
+ * Class RegisterController
+ *
+ * @package App\Http\Controllers\projects
+ */
 class RegisterController extends Controller
 {
     /**
-     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
-    public function display(Request $request)
+    public function display()
     {
         return view('projects.register.main');
     }
     
+    /**
+     * @param \App\Http\Requests\RegisterRequest $request
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function register(RegisterRequest $request)
     {
         $validated = $request->validated();
@@ -27,7 +34,7 @@ class RegisterController extends Controller
         $validated['password'] = Hash::make($validated['password']);
         $newUser = new User($validated);
         $newUser->save();
-    
+        
         return view('projects.login.main');
     }
     
